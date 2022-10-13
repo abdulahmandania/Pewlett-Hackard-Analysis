124683fdea6cbe634ae815a38016ac08a8e0b746
Dania Abdulahman <111723067+abdulahmandania@users.noreply.github.com>
2022-10-12 21:28:56 -0500

    Update Employee_Database_challenge.sql

--- Pewlett-Hackard-Analysis/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Employee_Database_challenge.sql
@@ -31,3 +31,20 @@ GROUP BY ut.title
 ORDER BY ut.count DESC;
 
 SELECT * FROM retiring_titles;
+
+-- Deliverable 2 
+
+SELECT DISTINCT ON (e.emp_no)	
+		e.emp_no, e.first_name, e.last_name, e.birth_date, 
+		de.from_date, de.to_date,
+		t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
+SELECT COUNT (mentorship_eligibilty.emp_no) FROM mentorship_eligibilty;
